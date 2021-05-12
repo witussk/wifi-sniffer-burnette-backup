d87bda77273cca85741a41d11d7aac02a4e0e8c4
witussk <witkovsm@lsbu.ac.uk>
2021-05-12 19:38:34 +0100

    Custom version of Ray's code

--- Sniffing6.ino
+++ Sniffing6.ino
@@ -1,39 +1,49 @@
-// by Ray Burnette 20161013 compiled on Linux 16.3 using Arduino 1.6.12
+/* 
+Modified according to the MIT licensing agreement
+Original made by Ray Burnette, available at: https://github.com/witussk/wifi-sniffer-burnette-backup
+*/
 
 #include <ESP8266WiFi.h>
 #include "./functions.h"
 
+// Variable setup
 #define disable 0
 #define enable  1
-// uint8_t channel = 1;
 unsigned int channel = 1;
 
 void setup() {
   Serial.begin(57600);
-  Serial.printf("\n\nSDK version:%s\n\r", system_get_sdk_version());
-  Serial.println(F("ESP8266 mini-sniff by Ray Burnette http://www.hackster.io/rayburne/projects"));
+  Serial.println(F("Final Year Project - MAC Address sniff test"));
   Serial.println(F("Type:   /-------MAC------/-----WiFi Access Point SSID-----/  /----MAC---/  Chnl  RSSI"));
 
-  wifi_set_opmode(STATION_MODE);            // Promiscuous works only with station mode
+  // Wi-Fi Module mode setup
+  wifi_set_opmode(STATION_MODE);           
   wifi_set_channel(channel);
   wifi_promiscuous_enable(disable);
-  wifi_set_promiscuous_rx_cb(promisc_cb);   // Set up promiscuous callback
+  wifi_set_promiscuous_rx_cb(promisc_cb);   
   wifi_promiscuous_enable(enable);
 }
 
+// Main loop, loops through individual Wi-Fi Channels to scan for MAC addresses
 void loop() {
   channel = 1;
   wifi_set_channel(channel);
+
   while (true) {
-    nothing_new++;                          // Array is not finite, check bounds and adjust if required
+    nothing_new++;                         
     if (nothing_new > 200) {
       nothing_new = 0;
       channel++;
-      if (channel == 15) break;             // Only scan channels 1 to 14
+
+      // Scans all 14 2.4 GHz channels
+      if (channel == 15) break;             
       wifi_set_channel(channel);
     }
-    delay(1);  // critical processing timeslice for NONOS SDK! No delay(0) yield()
-    // Press keyboard ENTER in console with NL active to repaint the screen
+
+    // Delay needed not to overload the SDK
+    delay(1);
+
+    // When serial enabled
     if ((Serial.available() > 0) && (Serial.read() == '\n')) {
       Serial.println("\n-------------------------------------------------------------------------------------\n");
       for (int u = 0; u < clients_known_count; u++) print_client(clients_known[u]);
@@ -42,4 +52,3 @@ void loop() {
     }
   }
 }
-
